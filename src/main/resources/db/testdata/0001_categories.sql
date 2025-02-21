f1e080ab1be3fb71d5efd982c4c298f04d51f930
Ana <adamikana@gmail.com>
2025-02-21 12:04:26 +0100

    feat: Add test data for Dish and Category

---
+++ src/main/resources/db/testdata/0001_categories.sql
@@ -0,0 +1,11 @@
+insert into category (name) values ('Śniadania');
+insert into category (name) values ('Obiady');
+insert into category (name) values ('Kolacje');
+insert into category (name) values ('Desery');
+insert into category (name) values ('Przekąski');
+insert into category (name) values ('Dania wegetariańskie');
+insert into category (name) values ('Dania wegańskie');
+insert into category (name) values ('Dania bezglutenowe');
+insert into category (name) values ('Przekąski na wynos');
+insert into category (name) values ('Dania niskokaloryczne');
+
