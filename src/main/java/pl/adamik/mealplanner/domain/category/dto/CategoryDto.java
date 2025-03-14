ad69f16d6387b33b4c13152f5937f00d04149e8d
Ana <adamikana@gmail.com>
2025-03-14 23:32:50 +0100

    refactor: CategoryDto lombok

--- src/main/java/pl/adamik/mealplanner/domain/category/dto/CategoryDto.java
+++ src/main/java/pl/adamik/mealplanner/domain/category/dto/CategoryDto.java
@@ -1,30 +1,15 @@
 package pl.adamik.mealplanner.domain.category.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
     private Long id;
     private String name;
-
-    public CategoryDto() {
-    }
-
-    public CategoryDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
