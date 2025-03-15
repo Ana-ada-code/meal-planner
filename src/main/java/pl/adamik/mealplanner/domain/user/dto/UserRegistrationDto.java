92afd9219395f2b4109b0eba24857dd7c640e359
Ana <adamikana@gmail.com>
2025-03-15 21:59:23 +0100

    refactor: UserRegistrationDto lombok

--- src/main/java/pl/adamik/mealplanner/domain/user/dto/UserRegistrationDto.java
+++ src/main/java/pl/adamik/mealplanner/domain/user/dto/UserRegistrationDto.java
@@ -1,30 +1,15 @@
 package pl.adamik.mealplanner.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationDto {
     private String email;
     private String password;
-
-    public UserRegistrationDto() {
-    }
-
-    public UserRegistrationDto(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
