3c7f5e9606ebf06beb8da718bb14643828f11b6c
Ana <adamikana@gmail.com>
2025-03-15 22:36:07 +0100

    refactor: User and UserRole lombok + builder

--- src/main/java/pl/adamik/mealplanner/domain/user/User.java
+++ src/main/java/pl/adamik/mealplanner/domain/user/User.java
@@ -1,6 +1,7 @@
 package pl.adamik.mealplanner.domain.user;
 
 import jakarta.persistence.*;
+import lombok.*;
 import pl.adamik.mealplanner.domain.dishselection.DishSelection;
 import pl.adamik.mealplanner.domain.favorite.Favorite;
 
@@ -9,6 +10,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
@@ -17,6 +23,7 @@ public class User {
     private Long id;
     private String email;
     private String password;
+    @Builder.Default
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
@@ -24,57 +31,10 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private Set<UserRole> roles = new HashSet<>();
+    @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<DishSelection> dishSelections = new ArrayList<>();
+    @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<Favorite> favorites = new ArrayList<>();
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<UserRole> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<UserRole> roles) {
-        this.roles = roles;
-    }
-
-    public List<DishSelection> getDishSelections() {
-        return dishSelections;
-    }
-
-    public void setDishSelections(List<DishSelection> dishSelections) {
-        this.dishSelections = dishSelections;
-    }
-
-    public List<Favorite> getFavorites() {
-        return favorites;
-    }
-
-    public void setFavorites(List<Favorite> favorites) {
-        this.favorites = favorites;
-    }
 }
--- src/main/java/pl/adamik/mealplanner/domain/user/UserRole.java
+++ src/main/java/pl/adamik/mealplanner/domain/user/UserRole.java
@@ -1,7 +1,14 @@
 package pl.adamik.mealplanner.domain.user;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class UserRole {
     @Id
@@ -9,28 +16,4 @@ public class UserRole {
     private Long id;
     private String name;
     private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
