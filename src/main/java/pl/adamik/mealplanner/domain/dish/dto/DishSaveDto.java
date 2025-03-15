59c73a4952c803a306001a07f2ec7de58a3fde22
Ana <adamikana@gmail.com>
2025-03-15 21:55:00 +0100

    refactor: DishSaveDto lombok

--- src/main/java/pl/adamik/mealplanner/domain/dish/dto/DishSaveDto.java
+++ src/main/java/pl/adamik/mealplanner/domain/dish/dto/DishSaveDto.java
@@ -3,8 +3,12 @@ package pl.adamik.mealplanner.domain.dish.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+@Getter
+@Setter
 public class DishSaveDto {
     private Long id;
     @NotBlank(message = "Nazwa dania nie może być pusta")
@@ -17,52 +21,4 @@ public class DishSaveDto {
     @NotNull(message = "Kategorie jest nie mogą być puste")
     private String category;
     private MultipartFile image;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(String recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public MultipartFile getImage() {
-        return image;
-    }
-
-    public void setImage(MultipartFile image) {
-        this.image = image;
-    }
 }
